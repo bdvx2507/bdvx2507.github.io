42a956e64db86c15c44c95c8c889c1fc5a772078
bdvx <bdv2507@gmail.com>
2015-11-15 19:02:32 +0300

    GLOBAL CHANGE

--- script.js
+++ script.js
@@ -1,17 +1,6 @@
-import System.IO;
- var fileName = "counter.txt";
 function modify_qty(val) {
-	
-	
-	 // read from filename
-     var sr = new StreamReader(Application.dataPath + "/" + fileName);
-     var fileContents = sr.ReadToEnd();
-     sr.Close();
-  
-      // put data from filename into a variable
-     var qty = fileContents.Split("\n"[0]);
-    //var qty = document.getElementById('qty').value;
-    var new_qty = parseInt(qty[0],10) + val;
+    var qty = document.getElementById('qty').value;
+    var new_qty = parseInt(qty,10) + val;
     
     if (new_qty < 0) {
         new_qty = 0;
@@ -21,5 +10,3 @@ function modify_qty(val) {
   return new_qty;
 }
 
- 
-  
