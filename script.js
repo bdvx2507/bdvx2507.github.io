895c45a9fdb7d54e854d7a49f4dbf5c09ddc6bda
bdvx <bdv2507@gmail.com>
2015-11-15 15:55:48 +0300

    try script

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
+import System.IO;
+ var fileName = "counter.txt";
 function modify_qty(val) {
-    var qty = document.getElementById('qty').value;
+	
+	
+	 // read from filename
+     var sr = new StreamReader(Application.dataPath + "/" + fileName);
+     var fileContents = sr.ReadToEnd();
+     sr.Close();
+  
+      // put data from filename into a variable
+     var qty = fileContents.Split("\n"[0]);
+    //var qty = document.getElementById('qty').value;
     var new_qty = parseInt(qty,10) + val;
     
     if (new_qty < 0) {
@@ -7,5 +18,8 @@ function modify_qty(val) {
     }
     
     document.getElementById('qty').value = new_qty;
-    return new_qty;
+  return new_qty;
 }
+
+ 
+  
